efc8bcf0205d6f1ebb1d60dde2e5dc16541068cf
Ibrahim Filali <brahimfilali758@gmail.com>
2023-11-17 00:11:55 +0100

    Fix environnement

---
+++ chess_frontend/src/App.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import logo from "./logo.svg";
+import styles from "./App.module.css";
+import Button from "@mui/material/Button";
+function App() {
+  return <div className={styles.app}></div>;
+}
+
+export default App;
